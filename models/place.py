f2633a8a9c685b3f925bb58b227d3cc7226578c6
Jesica-akeyo <jessica.akeyo@gmail.com>
2022-08-07 15:20:41 +0000

    HBNB

--- models/place.py
+++ models/place.py
@@ -6,15 +6,4 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Defines Place class"""
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_bathrooms = 0
-    number_rooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    """Defines Place class""
